6be84688d5a9f33dd09cf684a04b9e01abb6e0b5
Nicholas Cole <nicholas.cole@history.ox.ac.uk>
2020-03-03 10:18:14 +0000

    Change setup.py licence note to BSD

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=['richtextpy'],
     packages=find_packages(),
     include_package_data=True,
-    license='Private Code',
+    license='BSD',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 3',
